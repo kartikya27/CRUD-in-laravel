cf216f085a8826a657e47cf1eedfb7bd799a904c
Kartik Mandothiya <55804679+kartikya27@users.noreply.github.com>
2022-02-26 22:56:54 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,17 +1,16 @@
 <?php
 $key = '1214Mr9onMRUBjLRKnAUSrYNzZu9O9YV6J0h4gXHCU7K621a3daaa1616';
-$url = 'https://portfolio.wrkva.xyz/laravel-crud/crudApp/Api/Response/1214Mr9onMRUBjLRKnAUSrYNzZu9O9YV6J0h4gXHCU7K621a3daaa1616';
-echo $url;
+$url = 'https://portfolio.wrkva.xyz/laravel-crud/crudApp/Api/Response/'.$key;
+// echo $url;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 $response = curl_exec($ch);
-
-if ($response === false) 
-    $response = curl_error($ch);
-
-echo stripslashes($response);
-
 curl_close($ch);
+echo ($response);
+
+
 
 ?>
