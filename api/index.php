92bda9eb26865b038271676107d887bab77970cc
Kartik Mandothiya <kartikya27@gmail.com>
2022-02-26 22:47:46 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@ $url = 'https://portfolio.wrkva.xyz/laravel-crud/crudApp/Api/Response/1214Mr9onM
 // echo $url;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 curl_close($ch);
 
