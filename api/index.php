e07c906c5b1ea413c5e7c8487ce9ef820a3cf8f6
Kartik Mandothiya <kartikya27@gmail.com>
2022-02-26 22:56:28 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,13 +1,17 @@
 <?php
 $key = '1214Mr9onMRUBjLRKnAUSrYNzZu9O9YV6J0h4gXHCU7K621a3daaa1616';
 $url = 'https://portfolio.wrkva.xyz/laravel-crud/crudApp/Api/Response/1214Mr9onMRUBjLRKnAUSrYNzZu9O9YV6J0h4gXHCU7K621a3daaa1616';
-// echo $url;
+echo $url;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-curl_close($ch);
+$response = curl_exec($ch);
 
-echo $result;
+if ($response === false) 
+    $response = curl_error($ch);
+
+echo stripslashes($response);
+
+curl_close($ch);
 
 ?>
