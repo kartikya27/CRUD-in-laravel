5f0de418e662799a0b2e5d636cbb1c939cab6354
Kartik Mandothiya <kartikya27@gmail.com>
2022-02-26 22:25:48 +0530

    api int

---
+++ api/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$url = 'https://portfolio.wrkva.xyz/laravel-crud/crudApp/login/Api/Response/';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+$result = curl_exec($ch);
+curl_close($ch);
+
+return $result;
+
+?>
