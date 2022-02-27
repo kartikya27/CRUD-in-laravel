e7654113c593b45a99e8311190b8a151f36029b2
Kartik Mandothiya <kartikya27@gmail.com>
2022-02-27 15:19:28 +0530

    Update ApiLoginResponse.php

--- crudApp/app/Http/Controllers/ApiLoginResponse.php
+++ crudApp/app/Http/Controllers/ApiLoginResponse.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\ApiProvider;
+use Illuminate\Support\Facades\Hash;
 
 class ApiLoginResponse extends Controller
 {
