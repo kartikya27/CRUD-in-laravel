ba660a29e07d1a86e323100b3abb47d28843325d
Kartik Mandothiya <kartikya27@gmail.com>
2022-02-27 14:51:40 +0530

    Update ApiLoginResponse.php

--- crudApp/app/Http/Controllers/ApiLoginResponse.php
+++ crudApp/app/Http/Controllers/ApiLoginResponse.php
@@ -13,15 +13,15 @@ class ApiLoginResponse extends Controller
     {   
         $protectedData['key'] =$Authkey;
         $protectedData['secret'] =$secret;
-        // return json_encode($Auth);
+        
         header('Content-Type:application/json');
 
         $checkAuth = ApiProvider::where(['key'=>$protectedData['key']])->first();
         $secretAuth = ApiProvider::where(['scret'=>$protectedData['secret']]);
         // $checkAuth || Hash::make($protectedData['secret'],$checkAuth->AuthSecret);
-        if($checkAuth && $secretAuth)
-        // if($checkAuth || Hash::make($protectedData['secret'],$checkAuth->AuthSecret))
-        { 
+        // if($checkAuth && $secretAuth)
+        if($checkAuth || Hash::make($protectedData['secret'],$checkAuth->AuthSecret))
+        {
         $data['Status'] =true;       
         $data['users'] = User::all();
         $data['Result'] = "Success";
